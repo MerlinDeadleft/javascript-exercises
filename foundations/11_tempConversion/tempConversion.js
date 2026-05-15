f3bc09b735797d7f851838112ed1b1ba9085f513
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-05-15 15:32:56 +0200

    Finish foundations excercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    /* x °F = (x - 32) * (5/9) °C */
+    let c = (temp - 32) * (5/9);
+    return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    /* x °C = x * (9/5)⁠ + 32 °F */
+    let f = temp * (9/5) + 32;
+    return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
