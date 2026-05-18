c96e9888f5e36b4eaa8bfc00f361a5302db067c6
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-05-18 13:45:27 +0200

    Finish foundations excercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+    if(numbers.length === 0) {
+        return 0;
+    }
+    
+	return numbers.reduce((sum, item) => sum + item);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+    if(numbers.length === 0) {
+        return 0;
+    }
+    
+    return numbers.reduce((sum, item) => sum * item);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+    let result = 1;
+	for(let i = 1; i <= number; i++) {
+        result *= i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
