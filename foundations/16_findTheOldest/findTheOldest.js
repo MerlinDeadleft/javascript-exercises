29bcc0ab22b8efb3520768d66d21d232e5ff279b
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-05-18 14:35:17 +0200

    Finish foundations excercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const agesMap = arr.map(item => ({
+        person: item, age: ("yearOfDeath" in item ? item.yearOfDeath : new Date().getFullYear()) - item.yearOfBirth
+    }));
+    agesMap.sort((a, b) => b.age - a.age);
 
+
+    return agesMap[0].person;
 };
 
 // Do not edit below this line
