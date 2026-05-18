b8fdf1326a0c4db72eaeec354023a2fec5d23a27
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-05-18 14:09:50 +0200

    Finish foundations excercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const reversed = cleanedStr.split("").reverse().join("");
 
+    console.log(cleanedStr+"\n"+reversed);
+    return cleanedStr === reversed;
 };
 
 // Do not edit below this line
